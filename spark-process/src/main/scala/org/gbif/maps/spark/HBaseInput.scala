a55e8d632112d1ebc16c61f1ab9e1587f1ebca97
Matthew Blissett <mblissett@gbif.org>
2016-09-20 17:47:18 +0200

    Remove unnecessary Occurrence class, and generate schema/query parameters.

--- spark-process/src/main/scala/org/gbif/maps/spark/HBaseInput.scala
+++ spark-process/src/main/scala/org/gbif/maps/spark/HBaseInput.scala
@@ -10,31 +10,6 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import org.slf4j.LoggerFactory
 
-class Occurrence (
-  val rowkey: String,
-
-  val datasetKey: String,
-  val publishingOrgKey: String,
-  val publishingCountry: String,
-  val countryCode: String,
-  val basisOfRecord: String,
-
-  val decimalLatitude: Double,
-  val decimalLongitude: Double,
-  val kingdomKey: Int,
-  val phylumKey: Int,
-  val classKey: Int,
-
-  val orderKey: Int,
-  val familyKey: Int,
-  val genusKey: Int,
-  val speciesKey: Int,
-  val taxonKey: Int,
-
-  val year: Int,
-  val hasGeospatialIssues: Boolean
-) extends Serializable {}
-
 /**
   * Reads occurrence data from HBase, producing a DataFrame.
   */
@@ -67,52 +42,38 @@ object HBaseInput {
     conf.set(TableInputFormat.INPUT_TABLE, inputTable) // TODO Configure, not parameter
 
     conf.set(TableInputFormat.SCAN_COLUMN_FAMILY, "o")
-    conf.set(TableInputFormat.SCAN_COLUMNS,
-      "o:datasetKey o:publishingOrgKey o:publishingCountry o:countryCode o:basisOfRecord " +
-      "o:decimalLatitude o:decimalLongitude o:kingdomKey o:phylumKey o:classKey " +
-      "o:orderKey o:familyKey o:genusKey o:speciesKey o:taxonKey " +
-      "o:year o:hasGeospatialIssues")
+
+    // Fields to read from HBase, and their types as they will be in the DataFrame
+    val fieldNamesAndTypes : List[(String, DataType)] = List(
+      ("datasetKey", StringType),
+      ("publishingOrgKey", StringType),
+      ("publishingCountry", StringType),
+      ("countryCode", StringType),
+      ("basisOfRecord", StringType),
+
+      ("decimalLatitude", DoubleType),
+      ("decimalLongitude", DoubleType),
+      ("kingdomKey", IntegerType),
+      ("phylumKey", IntegerType),
+      ("classKey", IntegerType),
+
+      ("orderKey", IntegerType),
+      ("familyKey", IntegerType),
+      ("genusKey", IntegerType),
+      ("speciesKey", IntegerType),
+      ("taxonKey", IntegerType),
+
+      ("year", IntegerType),
+      ("hasGeospatialIssues", BooleanType)
+    )
+
+    val scanColumns = fieldNamesAndTypes.map(_._1).foldLeft("")( (a, b) => a + "o:"+b+" " ).trim
+    conf.set(TableInputFormat.SCAN_COLUMNS, scanColumns)
 
     val hBaseRDD = sc.newAPIHadoopRDD(conf,
       classOf[TableInputFormat],
       classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
       classOf[org.apache.hadoop.hbase.client.Result])
-
-    val allOccurrences = hBaseRDD.map(x => { // TODO x?
-      val r = x._2 // TODO r?
-      val keyRow = Bytes.toString(r.getRow())
-
-      val o = Bytes.toBytes("o");
-      val empty : Array[Byte] = new Array[Byte](4)
-
-      // TODO Don't make an Occurrence, only to make something else straight afterwards.
-
-      val row = new Occurrence(
-        keyRow,
-
-        Bytes.toString(Option(r.getValue(o, Bytes.toBytes("datasetKey"))).getOrElse(empty)),
-        Bytes.toString(Option(r.getValue(o, Bytes.toBytes("publishingOrgKey"))).getOrElse(empty)),
-        Bytes.toString(Option(r.getValue(o, Bytes.toBytes("publishingCountry"))).getOrElse(empty)),
-        Bytes.toString(Option(r.getValue(o, Bytes.toBytes("countryCode"))).getOrElse(empty)),
-        Bytes.toString(Option(r.getValue(o, Bytes.toBytes("basisOfRecord"))).getOrElse(empty)),
-
-        Bytes.toDouble(Option(r.getValue(o, Bytes.toBytes("decimalLatitude"))).getOrElse(new Array[Byte](8))),
-        Bytes.toDouble(Option(r.getValue(o, Bytes.toBytes("decimalLongitude"))).getOrElse(new Array[Byte](8))),
-        Bytes.toInt(Option(r.getValue(o, Bytes.toBytes("kingdomKey"))).getOrElse(empty)),
-        Bytes.toInt(Option(r.getValue(o, Bytes.toBytes("phylumKey"))).getOrElse(empty)),
-        Bytes.toInt(Option(r.getValue(o, Bytes.toBytes("classKey"))).getOrElse(empty)),
-
-        Bytes.toInt(Option(r.getValue(o, Bytes.toBytes("orderKey"))).getOrElse(empty)),
-        Bytes.toInt(Option(r.getValue(o, Bytes.toBytes("familyKey"))).getOrElse(empty)),
-        Bytes.toInt(Option(r.getValue(o, Bytes.toBytes("genusKey"))).getOrElse(empty)),
-        Bytes.toInt(Option(r.getValue(o, Bytes.toBytes("speciesKey"))).getOrElse(empty)),
-        Bytes.toInt(Option(r.getValue(o, Bytes.toBytes("taxonKey"))).getOrElse(empty)),
-
-        Bytes.toInt(Option(r.getValue(o, Bytes.toBytes("year"))).getOrElse(empty)),
-        Bytes.toBoolean(Option(r.getValue(o, Bytes.toBytes("hasGeospatialIssues"))).getOrElse(new Array[Byte](1)))
-      )
-      (keyRow, row)
-    })
 
 //    val filteredOccurrences = filteredOccurrences.filter(row => {
 //      val occ = row._2
@@ -121,67 +82,46 @@ object HBaseInput {
 //      !occ.hasGeospatialIssues
 //    })
 
-    allOccurrences.take(20).foreach(row => logger.info("Sample: {}", row._2))
-
-    val types : List[DataType] = List(
-      StringType,
-      StringType,
-      StringType,
-      StringType,
-      StringType,
-
-      DoubleType,
-      DoubleType,
-      IntegerType,
-      IntegerType,
-      IntegerType,
-
-      IntegerType,
-      IntegerType,
-      IntegerType,
-      IntegerType,
-      IntegerType,
-
-      IntegerType,
-      BooleanType)
-
     // Creating an SQL DataFrame
-    // TODO explain this!
-    val fields = ("datasetKey publishingOrgKey publishingCountry countryCode basisOfRecord " +
-      "decimalLatitude decimalLongitude kingdomKey phylumKey classKey " +
-      "orderKey familyKey genusKey speciesKey taxonKey " +
-      "year hasGeospatialIssues").toLowerCase.split(" ").zip(types).map{ case (fieldName: String, fieldType: DataType) => StructField(fieldName, fieldType, nullable = true)}
+    val fields = fieldNamesAndTypes.map{ case (fieldName, fieldType) => StructField(fieldName.toLowerCase(), fieldType, nullable = true) }
     val schema = StructType(fields)
-    val rowRDD = allOccurrences
-      .map(_._2)
-      .map(attributes => Row(
-        attributes.datasetKey,
-        attributes.publishingOrgKey,
-        attributes.publishingCountry,
-        attributes.countryCode,
-        attributes.basisOfRecord,
 
-        attributes.decimalLatitude,
-        attributes.decimalLongitude,
-        attributes.kingdomKey,
-        attributes.phylumKey,
-        attributes.classKey,
+    val empty = new Array[Byte](4)
+    val emptyDouble = new Array[Byte](8)
+    val emptyBoolean = new Array[Byte](1)
 
-        attributes.orderKey,
-        attributes.familyKey,
-        attributes.genusKey,
-        attributes.speciesKey,
-        attributes.taxonKey,
+    val rowRDD = hBaseRDD.map{ case(_, result) => {
+      val o = Bytes.toBytes("o");
 
-        attributes.year,
-        attributes.hasGeospatialIssues
-      ))
+      Row(
+        Bytes.toString(Option(result.getValue(o, Bytes.toBytes("datasetKey"))).getOrElse(empty)),
+        Bytes.toString(Option(result.getValue(o, Bytes.toBytes("publishingOrgKey"))).getOrElse(empty)),
+        Bytes.toString(Option(result.getValue(o, Bytes.toBytes("publishingCountry"))).getOrElse(empty)),
+        Bytes.toString(Option(result.getValue(o, Bytes.toBytes("countryCode"))).getOrElse(empty)),
+        Bytes.toString(Option(result.getValue(o, Bytes.toBytes("basisOfRecord"))).getOrElse(empty)),
+
+        Bytes.toDouble(Option(result.getValue(o, Bytes.toBytes("decimalLatitude"))).getOrElse(emptyDouble)),
+        Bytes.toDouble(Option(result.getValue(o, Bytes.toBytes("decimalLongitude"))).getOrElse(emptyDouble)),
+        Bytes.toInt(Option(result.getValue(o, Bytes.toBytes("kingdomKey"))).getOrElse(empty)),
+        Bytes.toInt(Option(result.getValue(o, Bytes.toBytes("phylumKey"))).getOrElse(empty)),
+        Bytes.toInt(Option(result.getValue(o, Bytes.toBytes("classKey"))).getOrElse(empty)),
+
+        Bytes.toInt(Option(result.getValue(o, Bytes.toBytes("orderKey"))).getOrElse(empty)),
+        Bytes.toInt(Option(result.getValue(o, Bytes.toBytes("familyKey"))).getOrElse(empty)),
+        Bytes.toInt(Option(result.getValue(o, Bytes.toBytes("genusKey"))).getOrElse(empty)),
+        Bytes.toInt(Option(result.getValue(o, Bytes.toBytes("speciesKey"))).getOrElse(empty)),
+        Bytes.toInt(Option(result.getValue(o, Bytes.toBytes("taxonKey"))).getOrElse(empty)),
+
+        Bytes.toInt(Option(result.getValue(o, Bytes.toBytes("year"))).getOrElse(empty)),
+        Bytes.toBoolean(Option(result.getValue(o, Bytes.toBytes("hasGeospatialIssues"))).getOrElse(emptyBoolean))
+      )
+    }}
+
     val df : DataFrame = sqlContext.createDataFrame(rowRDD, schema)
 
     // TODO Explain why this doesn't work.
-//    val df : DataFrame = sqlContext.createDataFrame(xxx.values, classOf[Occurrence])
+    // val df : DataFrame = sqlContext.createDataFrame(xxx.values, classOf[Occurrence])
 
-    allOccurrences.take(20).foreach(row => logger.info("Sample 2: {}", row._2))
     logger.error("Columns are {}", df.columns)
 
     return df
