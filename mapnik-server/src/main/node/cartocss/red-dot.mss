68226745147de456dfb5388444c1663b1c8c4bae
Matthew Blissett <mblissett@gbif.org>
2017-05-18 16:08:52 +0200

    Support much of the V1 map tile API.

---
+++ mapnik-server/src/main/node/cartocss/red-dot.mss
@@ -0,0 +1,8 @@
+/*
+ * A very simple high contrast (red or nothing) style, imitating the original V1 maps.
+ */
+
+#occurrence {
+  dot-width: 2;
+  dot-fill: #cc0000;
+}
--- mapnik-server/src/main/node/server.js
+++ mapnik-server/src/main/node/server.js
@@ -1,12 +1,12 @@
-var mapnik = require('mapnik')
-  , mercator = require('./sphericalmercator')
-  , request = require('request')
-  , http = require('http')
-  , url = require('url')
-  , fs = require('fs')
-  , yaml = require('yaml-js')
-  , gbifServiceRegistry = require('./gbifServiceRegistry')
-  , parser = require('./cartoParser');
+const mapnik = require('mapnik')
+    , mercator = require('./sphericalmercator')
+    , request = require('request')
+    , http = require('http')
+    , url = require('url')
+    , fs = require('fs')
+    , yaml = require('yaml-js')
+    , gbifServiceRegistry = require('./gbifServiceRegistry')
+    , parser = require('./cartoParser');
 
 /**
  * Compile the CartoCss into Mapnik stylesheets into a lookup dictionary
@@ -21,8 +21,10 @@ namedStyles["blue.marker"] = compileStylesheetSync("./cartocss/blue-marker.mss")
 namedStyles["outline.poly"] = compileStylesheetSync("./cartocss/outline-poly.mss")
 namedStyles["greenHeat.point"] = compileStylesheetSync("./cartocss/green-heat-dot.mss")
 namedStyles["purpleYellow.point"] = compileStylesheetSync("./cartocss/purple-yellow-dot.mss")
+namedStyles["purpleWhite.point"] = compileStylesheetSync("./cartocss/purple-white-dot.mss")
+namedStyles["red.point"] = compileStylesheetSync("./cartocss/red-dot.mss")
 function compileStylesheetSync(filename) {
-  // snippet simulating a tilejson response from tilelive, required only to give the layers for the cartoParser
+  // snippet simulating a TileJSON response from Tilelive, required only to give the layers for the CartoParser
   var tilejson = {
     data: {
       "vector_layers": [
@@ -86,6 +88,161 @@ function parseUrl(parsedRequest) {
   throw Error("URL structure is invalid, expected /some/map/type/{z}/{x}/{y}@{n}x.png?srs=EPSG:4326&...");
 }
 
+/**
+ * Parse /v1 GBIF API URLs, and handle as much as we can.
+ *
+ * This is not my nicest code.  Hopefully you don't need to touch it.
+ */
+function v1ParseUrl(parsedRequest) {
+  var z = parseInt(parsedRequest.query.z);
+  var x = parseInt(parsedRequest.query.x);
+  var y = parseInt(parsedRequest.query.y);
+
+  var type = parsedRequest.query.type;
+  var key = parsedRequest.query.key;
+
+  var mapKey;
+  if (type == "TAXON") mapKey = "taxonKey="+key
+  else if (type == "DATASET") mapKey = "datasetKey="+key
+  else if (type == "PUBLISHER") mapKey = "publishingOrganizationKey="+key
+  else if (type == "COUNTRY") mapKey = "country="+key
+  else mapKey = null;
+
+  var basisOfRecord = new Set();
+
+  // Year ranges.  We assume ranges given are continuous, but don't permit showing no-year records as well as a range —
+  // unless the whole range (pre-1900 to 2020) is selected, then no year filter is requested.
+  var
+    obsStart = 9999,
+    obsEnd = -1,
+    spStart = 9999,
+    spEnd = -1,
+    othStart = 9999,
+    othEnd = -1;
+  var noYear = false;
+  var obs, sp, oth;
+
+  var layers = parsedRequest.query.layer;
+  if (!Array.isArray(layers)) {
+    layers = [layers];
+  }
+
+  for (var l of layers) {
+    if (l == "LIVING") {
+      basisOfRecord.add("LIVING_SPECIMEN");
+    } else if (l == "FOSSIL") {
+      basisOfRecord.add("FOSSIL_SPECIMEN");
+    } else {
+       var first_ = l.indexOf('_');
+       var second_ = l.indexOf('_', first_+1);
+
+       var prefix = l.substring(0, first_);
+       var startYear = l.substring(first_+1, second_);
+       var endYear = l.substring(second_+1);
+
+       if (startYear == "NO") {
+         noYear = true;
+       } else if (startYear == "PRE") {
+         startYear = 0;
+       }
+
+       switch (prefix) {
+         case "OBS":
+           basisOfRecord.add("OBSERVATION");
+           basisOfRecord.add("HUMAN_OBSERVATION");
+           basisOfRecord.add("MACHINE_OBSERVATION");
+           if (startYear != "NO") {
+             obsStart = Math.min(obsStart, startYear);
+             obsEnd = Math.max(obsEnd, endYear);
+           }
+           obs = true;
+           break;
+         case "SP":
+           basisOfRecord.add("PRESERVED_SPECIMEN");
+           if (startYear != "NO") {
+             spStart = Math.min(spStart, startYear);
+             spEnd = Math.max(spEnd, endYear);
+           }
+           sp = true;
+           break;
+         case "OTH":
+           basisOfRecord.add("MATERIAL_SAMPLE");
+           basisOfRecord.add("LITERATURE");
+           basisOfRecord.add("UNKNOWN");
+           if (startYear != "NO") {
+             othStart = Math.min(othStart, startYear);
+             othEnd = Math.max(othEnd, endYear);
+           }
+           oth = true;
+           break;
+         default:
+       }
+     }
+  }
+
+  // If all are selected, don't filter.
+  if (basisOfRecord.size == 9) {
+    basisOfRecord.clear();
+  }
+
+  var year;
+
+  // All year filters must apply to all record types.
+  var yearsMismatch = false;
+  yearsMismatch &= obs && sp  && (obsStart !=  spStart || obsEnd !=  spEnd);
+  yearsMismatch &= obs && oth && (obsStart != othStart || obsEnd != othEnd);
+  yearsMismatch &=  sp && oth && ( spStart != othStart ||  spEnd != othEnd);
+
+  if (!yearsMismatch) {
+    if (obs && obsStart == 9999 || sp && spStart == 9999 || oth && othStart == 9999) {
+      year = null;
+    } else if (obs) {
+      year = obsStart + "," + obsEnd;
+    } else if (sp) {
+      year = spStart + "," + spEnd;
+    } else if (oth) {
+      year = othStart + "," + othEnd;
+    } else {
+      // Only fossils and/or living
+      year = null;
+    }
+  } else {
+    var detail = "OBS "+obsStart+"-"+obsEnd+"; "+
+        "SP "+spStart+"-"+spEnd+"; "+
+        "OTH "+othStart+"-"+othEnd+"\n";
+
+    throw Error("Start and end years must be the same for each layer (BasisOfRecord): "+detail);
+  }
+
+  if (year == "0,2020" && noYear) {
+    year = null;
+  } else if (year && noYear) {
+    throw Error("Can't display undated records as well as a range of dated ones.\n");
+  }
+
+  var stylesheet = namedStyles['classic.point'];
+
+  if (parsedRequest.query.saturation == "true") {
+    stylesheet = namedStyles['purpleWhite.point'];
+  } else if (parsedRequest.query.colors == ",,#CC0000FF") {
+    stylesheet = namedStyles['red.point'];
+  }
+
+  // This gets us 256 pixel tiles.
+  var density = 0.5;
+
+  return {
+    "z": z,
+    "x": x,
+    "y": y,
+    "density": density,
+    "stylesheet": stylesheet,
+    "year": year,
+    "key": mapKey,
+    "basisOfRecord": Array.from(basisOfRecord)
+  }
+}
+
 function vectorRequest(parsedRequest) {
   // reformat the request to the type expected by the VectorTile Server
   // Remove raster parameters, to improve cacheability
@@ -95,6 +252,33 @@ function vectorRequest(parsedRequest) {
   parsedRequest.pathname = parsedRequest.pathname.replace("@2x.png", ".mvt");
   parsedRequest.pathname = parsedRequest.pathname.replace("@3x.png", ".mvt");
   parsedRequest.pathname = parsedRequest.pathname.replace("@4x.png", ".mvt");
+  parsedRequest.hostname = config.tileServer.host;
+  parsedRequest.port = config.tileServer.port;
+  parsedRequest.pathname = config.tileServer.prefix + parsedRequest.pathname;
+  parsedRequest.protocol = "http:";
+  return url.format(parsedRequest);
+}
+
+function v1VectorRequest(z, x, y, year, key, basisOfRecord, parsedRequest) {
+  delete parsedRequest.search; // Must be removed to force regeneration of query string
+
+  var params = "srs=EPSG:3857";
+
+  if (year) {
+    params += "&year=" + year;
+  }
+
+  if (basisOfRecord.length > 0) {
+    params += "&basisOfRecord=" + basisOfRecord.join("&basisOfRecord=");
+  }
+
+  if (key) {
+    params += "&" + key;
+  }
+
+  parsedRequest.search = params;
+
+  parsedRequest.pathname = "/map/occurrence/density/"+z+"/"+x+"/"+y+".mvt";
   parsedRequest.hostname = config.tileServer.host;
   parsedRequest.port = config.tileServer.port;
   parsedRequest.pathname = config.tileServer.prefix + parsedRequest.pathname;
@@ -116,15 +300,39 @@ function createServer(config) {
     } else {
       // Handle map tiles.
 
-      try {
-        var parameters = parseUrl(parsedRequest);
-      } catch (e) {
-        res.writeHead(400, { 'Content-Type': 'text/plain' });
-        res.end(e.message);
-        return;
+      var parameters, vectorTileUrl;
+      if (parsedRequest.pathname.indexOf('tile.png') > 0) {
+        try {
+          console.log("Legacy try");
+          parameters = v1ParseUrl(parsedRequest);
+          vectorTileUrl = v1VectorRequest(parameters.z, parameters.x, parameters.y, parameters.year, parameters.key, parameters.basisOfRecord, parsedRequest);
+        } catch (e) {
+          res.writeHead(410, {
+            'Content-Type': 'image/png',
+            'Access-Control-Allow-Origin': '*',
+            'X-Error': "Some features of GBIF's V1 map API have been deprecated.  See http://www.gbif.org/developer/maps for further information.",
+            'X-Error-Detail': e.message
+          });
+          res.end(fs.readFileSync('./public/map/deprecated-warning-tile.png'));
+          console.log("V1 request failed", e.message);
+          return;
+        }
+      } else {
+        try {
+          parameters = parseUrl(parsedRequest);
+          vectorTileUrl = vectorRequest(parsedRequest);
+        } catch (e) {
+          res.writeHead(400, {
+            'Content-Type': 'image/png',
+            'Access-Control-Allow-Origin': '*',
+            'X-Error': "Unable to parse request; see http://www.gbif.org/developer/maps for information.",
+            'X-Error-Detail': e.message
+          });
+          res.end(fs.readFileSync('./public/map/400.png'));
+          console.log("V2 request failed", e.message);
+          return;
+        }
       }
-
-      var vectorTileUrl = vectorRequest(parsedRequest);
 
       // issue the request to the vector tile server and render the tile as a PNG using Mapnik
       console.log("Fetching vector tile:", vectorTileUrl);
