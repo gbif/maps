8cfcd91a24ba0a5cd0da9d059badeccc80c3c57c
Matthew Blissett <mblissett@gbif.org>
2018-06-21 13:52:14 +0200

    Script to deploy Oozie workflows.

--- oozie-workflow/README.md
+++ oozie-workflow/README.md
@@ -2,8 +2,8 @@
 
 Oozie workflow for backfilling the maps.
 
-Example usage:
+Install using the script:
 
- - To build: ```mvn clean package```
- - Copy to HDFS: ```hdfs dfs -copyFromLocal target/maps-backfill-workflow-dev /```
- - To launch: ```oozie job --oozie http://c1n2.gbif.org:11000/oozie -config ./bin/dev.properties -run```
+```
+./install-workflow.sh dev points GITHUB_KEY
+```
---
+++ oozie-workflow/install-workflow.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+ENV=$1
+TOKEN=$2
+
+echo "Installing points and pyramid workflows for $ENV"
+
+echo "Get latest maps-coord config profiles from GitHub"
+curl -Ss -H "Authorization: token $TOKEN" -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/gbif/gbif-configuration/contents/maps-builder/$ENV/points.properties
+curl -Ss -H "Authorization: token $TOKEN" -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/gbif/gbif-configuration/contents/maps-builder/$ENV/tiles.properties
+
+P_START=$(date +%Y-%m-%d)T$(grep '^startHour=' points.properties | cut -d= -f 2)Z
+P_FREQUENCY=$(grep '^frequency=' points.properties | cut -d= -f 2)
+P_OOZIE=$(grep '^oozie.url=' points.properties | cut -d= -f 2)
+
+T_START=$(date +%Y-%m-%d)T$(grep '^startHour=' tiles.properties | cut -d= -f 2)Z
+T_FREQUENCY=$(grep '^frequency=' tiles.properties | cut -d= -f 2)
+T_OOZIE=$(grep '^oozie.url=' tiles.properties | cut -d= -f 2)
+
+# Gets the Oozie id of the current coordinator job if it exists
+WID=$(oozie jobs -oozie $OOZIE -jobtype coordinator -filter name=MapBuild-Points | awk 'NR==3 {print $1}')
+if [ -n "$WID" ]; then
+  echo "Killing current coordinator job" $WID
+  sudo -u hdfs oozie job -oozie $OOZIE -kill $WID
+fi
+
+WID=$(oozie jobs -oozie $OOZIE -jobtype coordinator -filter name=MapBuild-Tiles | awk 'NR==3 {print $1}')
+if [ -n "$WID" ]; then
+  echo "Killing current coordinator job" $WID
+  sudo -u hdfs oozie job -oozie $OOZIE -kill $WID
+fi
+
+echo "Assembling jar for $ENV"
+# Oozie uses timezone UTC
+mvn -Dpoints.frequency=$P_FREQUENCY -Dpoints.start=$P_START -Dtiles.frequency=$T_FREQUENCY -Dtiles.start=$T_START -DskipTests -Duser.timezone=UTC clean install
+
+echo "Copy to Hadoop"
+sudo -u hdfs hdfs dfs -rm -r /maps-backfill-workflow-$ENV/
+sudo -u hdfs hdfs dfs -copyFromLocal target/maps-backfill-workflow-$ENV /
+
+echo "Start Oozie points job"
+sudo -u hdfs oozie job --oozie $OOZIE -config points.properties -run
+sleep 5
+echo "Start Oozie tiles job"
+sudo -u hdfs oozie job --oozie $OOZIE -config tiles.properties -run
