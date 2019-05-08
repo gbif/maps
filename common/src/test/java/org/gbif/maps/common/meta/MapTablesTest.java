8e5ddea9df69c81161989e1b6cd08fe88e05adf3
Matthew Blissett <mblissett@gbif.org>
2019-05-08 12:27:02 +0200

    Oozie jobs now set to UTC.

--- common/src/test/java/org/gbif/maps/common/meta/MapTablesTest.java
+++ common/src/test/java/org/gbif/maps/common/meta/MapTablesTest.java
@@ -15,9 +15,8 @@ public class MapTablesTest {
     MapTables m2 = MapTables.deserialize(m1.serialize());
     assertEquals("Tables are not the same after SerDe", m1, m2);
 
-    // Test assumes the client's local timezone is Europe/Copenhagen (or equivalent).
-    assertEquals("Date has not been handled correctly", "2017-07-10T10:34Z", m1.getTileTableDate());
-    assertEquals("Date has not been handled correctly", "2017-02-10T11:34Z", m1.getPointTableDate());
+    assertEquals("Date has not been handled correctly", "2017-07-10T12:34Z", m1.getTileTableDate());
+    assertEquals("Date has not been handled correctly", "2017-02-10T12:34Z", m1.getPointTableDate());
   }
 
 }
