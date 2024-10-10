b5fe5ab67cbcc394212941c62b367b7f80cb295b
nvolik <nikolay.volik@hotmail.com>
2024-10-10 15:26:14 +0200

    Add IMAGE_LATEST variable

--- build/spark-generate-maps-docker-build.sh
+++ build/spark-generate-maps-docker-build.sh
@@ -3,6 +3,7 @@ MODULE="spark-generate-maps"
 
 POM_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
 IMAGE=docker.gbif.org/${MODULE}:${POM_VERSION}
+IMAGE_LATEST=docker.gbif.org/${MODULE}:latest
 
 echo "Building Docker image: ${IMAGE}"
 docker build -f ./${MODULE}/docker/Dockerfile ./${MODULE} --build-arg JAR_FILE=${MODULE}-${POM_VERSION}.jar -t ${IMAGE}
@@ -12,14 +13,14 @@ docker push ${IMAGE}
 
 if [[ $IS_M2RELEASEBUILD = true ]]; then
   echo "Updated latest tag poiting to the newly released ${IMAGE}"
-  docker tag ${IMAGE} docker.gbif.org/${MODULE}:latest
-  docker push docker.gbif.org/${MODULE}:latest
+  docker tag ${IMAGE} ${IMAGE_LATEST}
+  docker push ${IMAGE_LATEST}
 fi
 
 echo "Removing local Docker image: ${IMAGE}"
 docker rmi ${IMAGE}
 
 if [[ $IS_M2RELEASEBUILD = true ]]; then
-  echo "Removing local Docker image with latest tag: docker.gbif.org/${MODULE}:latest"
-  docker rmi docker.gbif.org/${MODULE}:latest
+  echo "Removing local Docker image with latest tag: ${IMAGE_LATEST}"
+  docker rmi ${IMAGE_LATEST}
 fi
